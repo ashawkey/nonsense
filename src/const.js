c4697ebb1ccc42f122fd1e3b8e0ddb917e9bb3a3
ashawkey <963113090@qq.com>
2020-09-03 19:44:22 +0800

    deploy

--- src/const.js
+++ src/const.js
@@ -1,2 +1,2 @@
-//export const API_ROOT = "https://www.hawia.xyz/api";
-export const API_ROOT = "http://127.0.0.1:5000/api/nonsense";
+//export const API_ROOT = "http://127.0.0.1:5000/api/nonsense";
+export const API_ROOT = "https://www.hawia.xyz/api/nonsense";
